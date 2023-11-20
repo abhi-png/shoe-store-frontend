3f4d8148e11c0bb067f50791441ad1cf136b2edf
Abinash Kar <abinash.kar611@gmail.com>
2023-11-20 19:34:43 +0530

    new

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home({ products }) {
       <main>
          <HeroBanner />
          <Wrapper>
-            {/* heading and paragaph start */}
+            {/* heading and paragaph starts */}
             <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
                <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
                   Cushioning for Your Miles
@@ -38,4 +38,5 @@ export async function getStaticProps() {
    return {
       props: { products },
    };
+   console.log(products);
 }
