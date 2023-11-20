b488c5300453b6d763e002b1910db5624bc47eb1
Abinash Kar <abinash.kar611@gmail.com>
2023-11-20 19:37:15 +0530

    new update

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home({ products }) {
       <main>
          <HeroBanner />
          <Wrapper>
-            {/* heading and paragaph starts */}
+            {/* heading and paragaph start */}
             <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
                <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
                   Cushioning for Your Miles
